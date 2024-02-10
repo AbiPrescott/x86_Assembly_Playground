0b4af25c54d434da6081c26fcb064f97335796c4
Abi Prescott <abiprescott586@gmail.com>
2024-02-10 09:08:38 +0400

    .

--- 2-maximum.s
+++ 2-maximum.s
@@ -1,29 +1,29 @@
 .section .data
-    data_items:
-        .long 3, 67, 34, 222, 45, 75, 54, 34, 44, 33, 22, 11, 66, 0
+data_items:
+        .quad 3, 67, 34, 222, 45, 75, 54, 34, 44, 33, 22, 11, 66, 0
 
 .section .text
 .globl _start
 
 #this section loads %eax with the xth digit of the array "data_items"
 _start:
-    movl $0, %edi
-    movl data_items(,%edi, 4), %eax
-    movl %eax, %ebx
+    mov $0, %rdi
+    mov data_items(,%rdi, 4), %rax
+    mov %rax, %rbx
 
 #this section loads %ebx with the current highest digit that has been encountered
 start_loop:
-    cmpl $0, %eax
+    cmp $1, %rax
     je loop_exit
-    incl %edi
-    movl data_items(,%edi, 4), %eax
-    cmpl %ebx, %eax
+    inc %rdi
+    mov data_items(,%rdi, 4), %rax
+    cmp %rbx, %rax
     jle start_loop
 
-    movl %eax, %ebx
+    mov %rax, %rbx
     jmp start_loop
 
 #this section exits the program. The value in %ebx will be shown when command 'echo $?' is executed
 loop_exit:
-    movl $1, %eax
+    mov $1, %rax
     int $0x80
